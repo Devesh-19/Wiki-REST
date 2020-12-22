b63c4f583720127b6b05073bbf43f4a1bea4aef9
Devesh-19 <devdeveshsingh@gmail.com>
2020-12-22 17:21:04 +0530

    Create article schema

--- app.js
+++ app.js
@@ -1,21 +1,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-mongoose.connect('mongodb://localhost/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect("mongodb://localhost/wikiDB", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+const articleSchema = {
+	title: String,
+	content: String,
+};
+
+const Article = mongoose.model("Article", articleSchema);
+
+app.use(
+	bodyParser.urlencoded({
+		extended: true,
+	})
+);
 app.use(express.static("public"));
 
 //TODO
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, function () {
+	console.log("Server started on port 3000");
 });
